8e6fdbf477b1174bad6ea8ca2bd507f0fe7f64a9
kirby@puppetlabs.com <mk.kirby@gmail.com>
2021-08-02 14:17:11 -0700

    Add ec-deployment module

---
+++ modules/ec-deployment/trafficfilter.tf
@@ -0,0 +1,14 @@
+resource "ec_deployment_traffic_filter" "gcp_vpc_nat" {
+  name   = "${var.project}_filter"
+  region = var.ec_region
+  type   = "ip"
+
+  rule {
+    source = var.traffic_filter_sourceip
+  }
+}
+
+resource "ec_deployment_traffic_filter_association" "ec_tf_association" {
+  traffic_filter_id = ec_deployment_traffic_filter.gcp_vpc_nat.id
+  deployment_id     = ec_deployment.ecproject.id
+}
---
+++ modules/ec-deployment/variables.tf
@@ -0,0 +1,31 @@
+variable "traffic_filter_sourceip" {
+  description = "traffic filter source IP"
+  type        = string
+  default     = ""
+}
+
+variable "ec_region" {
+# For a list of all cloud provider regions see https://www.elastic.co/guide/en/cloud/current/ec-regions-templates-instances.html
+  description = "cloud provider region"
+  type        = string
+  default     = "gcp-us-west1"
+}
+
+variable "deployment_templateid" {
+# List of deployment types can be found here https://www.elastic.co/guide/en/cloud/current/ec-create-deployment.html
+  description = "ID of Elastic Cloud deployment type"
+  type        = string
+  default     = "gcp-io-optimized"
+}
+
+variable "autoscale" {
+  description = "Enable autoscaling of elasticsearch"
+  type        = string
+  default     = "true"
+}
+
+variable "ec_stack_version" {
+  description = "Version of Elastic Cloud stack to deploy"
+  type        = string
+  default     = ""
+}
